27b9382f76aeeadd0a80d08134899be04873ff53
tayna.blasques <tayna.blasques@sovos.com>
2021-10-27 21:38:06 -0300

    pequenas alterações

--- conversoes.js
+++ conversoes.js
@@ -9,10 +9,18 @@ const numeroString = "456";
 
 //Number()
 //String()
-console.log(numero + Number(numeroString));
+console.log(numero + Number(numeroString)); //conversao explícita
 
 
 // "===" compara valor e tipo
 // "==" compara apenas valor
 
-//conversao explícita
+//Podemos usar o operador de soma + para fazer a conversão de textos para números, colocando-os antes das variáveis:
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
+
+let usuarioConectado= false;
+console.log (Number( usuarioConectado ) ); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+usuarioConectado= true;
+console.log (Number( usuarioConectado ) ); // agora teremos a conversão de true (verdadeiro) para o número 1.
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //var
 // funciona em qualquer parte do código
+//var tem um escopo global, então funciona em qualquer parte do código, o que pode gerar possíveis problemas em relação à lógica da aplicação.
 
-/* var altura = 5;
+var altura = 5;
 var comprimento = 7;
 
 area = altura * comprimento;
-console.log(area); */
+console.log(area);
 
 //nesse caso a variável area não precisou ser declarada pois o nodeJS ja declarou a variavel
 //porém faz parte de boas práticas declarar a variável
@@ -14,7 +15,7 @@ console.log(area); */
 //let e var não precisam ser inicializadas com um valor pré definido, ou seja, o valor pode ser alterado futuramente
 //let precisa ser declarada antes de ser inicializada
 
-/* let forma = "retangulo"
+let forma = "retangulo"
 let altura = 5;
 let comprimento = 7;
 let area;
@@ -25,7 +26,7 @@ if ( forma === "retangulo") { // 3 simbolos de "=" indicam que a comparação é
     area = (altura * comprimento) / 2;
 }
 
-console.log(area); */
+console.log(area);
 
 //const
 //sempre que se inicia uma const é necessário atribuir um valor
@@ -43,3 +44,16 @@ if ( forma === "quadrado") {
 }
 
 console.log(area);
+
+//OUTRO EXEMPLO COM CONST, onde o valor não muda como podemos ver no console.log
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
